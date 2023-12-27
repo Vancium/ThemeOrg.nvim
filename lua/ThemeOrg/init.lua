1999254878d3b37a15926fbb23b8f84cd7c5132b
Vancium <Natevance01@gmail.com>
2023-12-27 18:02:40 -0500

    Settings

--- lua/ThemeOrg/init.lua
+++ lua/ThemeOrg/init.lua
@@ -1,7 +1,14 @@
-local M = {}
+local ThemeOrg = {}
 
-M.setup = function()
-	print("setup")
+ThemeOrg.settings = {
+	name = "ThemeOrg",
+	version = "0.1",
+}
+
+ThemeOrg.setup = function(user_settings)
+	if user_settings then
+		ThemeOrg.settings = vim.tbl_deep_extend("force", ThemeOrg.settings, user_settings)
+	end
 end
 
-return M
+return ThemeOrg
